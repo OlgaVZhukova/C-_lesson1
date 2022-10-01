17999564844f95267c321f6464b866c723210e1f
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-01 13:09:19 +0300

    Example 005 IfElse added

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Привет, Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
